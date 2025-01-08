00b9faaaae14cb80f2a4325fb27fe05f09cde68f
Tim <119216750+meshwho@users.noreply.github.com>
2025-01-08 19:37:16 +0300

    Add files via upload

--- flaskSite/app.py
+++ flaskSite/app.py
@@ -9,14 +9,24 @@ def load_html_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
-@app.route('/')
+@app.route('/ua')
 def home():
     html_main = load_html_from_file('main_page.html')  # Укажите путь к вашему HTML-файлу
+    return render_template_string(html_main)
+
+@app.route('/en')
+def home_en():
+    html_main = load_html_from_file('main_page_en.html')  # Укажите путь к вашему HTML-файлу
     return render_template_string(html_main)
 
 @app.route('/transformer')
 def transformer():
     html_transformer = load_html_from_file('transformer_page.html')  # Укажите путь к вашему HTML-файлу
+    return render_template_string(html_transformer)
+
+@app.route('/transformer_en')
+def transformer_en():
+    html_transformer = load_html_from_file('transformer_page_en.html')  # Укажите путь к вашему HTML-файлу
     return render_template_string(html_transformer)
 
 if __name__ == '__main__':
---
+++ flaskSite/static/main.js
@@ -0,0 +1,10 @@
+import './styles/modern-normalize.css';
+import '../styles/components/header.css';
+import './styles/main_style.css';
+import './styles/utils.css';
+
+import mobilenav from "./utils/mobilenav";
+import mobileNav from "./utils/mobilenav";
+
+mobilenav();
+mobileNav();
